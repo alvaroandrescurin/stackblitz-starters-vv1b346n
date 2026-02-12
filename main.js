9bd753c4fbdc79c545efbd54603e12ac95286d26
alvaroandrescurin <alvaroandrescurin@gmail.com>
2026-02-12 11:37:21 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
-  // Esto le dice a Electron que cargue lo que ves en el navegador
-  win.loadURL('http://localhost:3000') 
+  // TRUCO: Si existe la carpeta "dist", carga el archivo. 
+  // Si no, intenta cargar el localhost (para cuando estés en StackBlitz).
+  const isProd = !process.env.VITE_DEV_URL;
+
+  if (app.isPackaged || isProd) {
+    win.loadFile(path.join(__dirname, 'dist/index.html'))
+  } else {
+    win.loadURL('http://localhost:3000')
+  }
 }
 
 app.whenReady().then(createWindow)
+
+// Cerrar cuando todas las ventanas se cierren (estándar de Windows)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
