b30b500d2950b3a6f44392c731c5bbc1c653f471
alvaroandrescurin <alvaroandrescurin@gmail.com>
2026-02-12 12:11:52 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: './', // <--- ESTO ARREGLA LA PANTALLA EN BLANCO
       server: {
         port: 3000,
         host: '0.0.0.0',
